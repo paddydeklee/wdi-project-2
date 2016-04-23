7b3a8da37575f80b1af231370318b48f346654cf
paddydeklee <paddydeklee@gmail.com>
2016-04-23 20:09:21 +0100

    creating second model case

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,47 @@ u1 = User.create!(email: "paddy@paddy.com",
                   last_name: "de Klee", 
                   image: "http://fillmurray.com/300/300", 
                   role: "admin")
+
+u2 = User.create!(email: "patient1@paddy.com", 
+                  password: "password", 
+                  username: "patient1", 
+                  first_name: "Patient", 
+                  last_name: "One", 
+                  image: "http://fillmurray.com/300/301", 
+                  role: "patient")
+
+
+u3 = User.create!(email: "patient2@paddy.com", 
+                  password: "password", 
+                  username: "patient2", 
+                  first_name: "Patient", 
+                  last_name: "Two", 
+                  image: "http://fillmurray.com/300/302", 
+                  role: "patient")
+
+
+u4 = User.create!(email: "patient3@paddy.com", 
+                  password: "password", 
+                  username: "patient3", 
+                  first_name: "Patient", 
+                  last_name: "Three", 
+                  image: "http://fillmurray.com/300/303", 
+                  role: "patient")
+
+
+u5 = User.create!(email: "doctor1@paddy.com", 
+                  password: "password", 
+                  username: "doctor1", 
+                  first_name: "Doctor", 
+                  last_name: "One", 
+                  image: "http://fillmurray.com/300/304", 
+                  role: "doctor")
+
+u6 = User.create!(email: "doctor2@paddy.com", 
+                  password: "password", 
+                  username: "doctor2", 
+                  first_name: "Doctor", 
+                  last_name: "Two", 
+                  image: "http://fillmurray.com/300/305", 
+                  role: "doctor")
+
